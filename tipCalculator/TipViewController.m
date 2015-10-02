c292a3ad50dc9e5ae51024cf9c2233227d5b248f
Steve Mitchell <smitchell@groupon.com>
2015-10-02 16:05:29 -0500

    set text to decimal, and adjust rounding for numbers like > .006

--- tipCalculator/TipViewController.m
+++ tipCalculator/TipViewController.m
@@ -60,7 +60,7 @@
     float tipAmount = billAmount * ([tipPercentages[self.tipControl.selectedSegmentIndex] floatValue]/100);
     if (self.roundControl.on) {
         //to correct for float precision errors, numbers within 1 penny of the floor with round down.
-        tipAmount = floor(tipAmount + 0.99);
+        tipAmount = floor(tipAmount + 0.994);
     }
     float totalAmount = billAmount + tipAmount;
     
